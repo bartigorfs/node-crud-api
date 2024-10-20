4a06de9cdb5fd68b38c503b324024462c5471a3a
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-20 16:39:07 +0300

    feat: memory errors tests

---
+++ src/models/memory.model.spec.ts
@@ -0,0 +1,25 @@
+import {MemInvalidArgs, MemNotFound} from "@/models/memory.model";
+
+describe('MemNotFound', () => {
+  test('should create an instance of MemNotFound', () => {
+    const error = new MemNotFound();
+
+    expect(error).toBeInstanceOf(MemNotFound);
+
+    expect(error.message).toBe('User or users not found!');
+
+    expect(error.name).toBe('MemNotFound');
+  });
+});
+
+describe('MemInvalidArgs', () => {
+  test('should create an instance of MemInvalidArgs', () => {
+    const error = new MemInvalidArgs();
+
+    expect(error).toBeInstanceOf(MemInvalidArgs);
+
+    expect(error.message).toBe('Invalid arguments!');
+
+    expect(error.name).toBe('MemInvalidArgs');
+  });
+});
